7535b9eba1c54e5c3e5c794f2cc97c56c994c7f6
Thauan <tholiveira@id.uff.bra>
2024-01-08 20:59:14 -0300

    Working in progress

--- src/services/general/auth/acess.js
+++ src/services/general/auth/acess.js
@@ -2,7 +2,6 @@ import api from "../utils/api";
 import { setTokenCookie, removeTokenCookie } from "../utils/cookie";
 
 export const login = async (credentials, _callback) => {
-  console.log("AAA", credentials);
     api
       .post(`patients/login`, credentials)
       .then((response) => {
