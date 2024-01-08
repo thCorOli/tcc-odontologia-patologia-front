7ad8d16c5489b9850344d22892c5e412752d5a5c
Thauan <tholiveira@id.uff.bra>
2024-01-08 19:59:32 -0300

    Finalizing page selection forms

--- src/pages/dentista/forms/components/index.js
+++ src/pages/dentista/forms/components/index.js
@@ -2,30 +2,39 @@ import styled, { css } from "styled-components";
 import "../../../../constants/colors.css";
 
 export const Card = styled.div`
-    width: 200px;
-    height: 200px;
-    display:flex;
-    flex-direction: column; /* Altera a direção para vertical */
-    justify-content: center; /* Alinha verticalmente */
-    align-items: center;    /* Alinha horizontalmente */
+    width: 220px; /* Aumentando a largura para acomodar ambos os elementos */
+    height: 220px; /* Ajustando a altura também */
+    display: flex;
+    transition: 0.4s;
+    flex-direction: column; 
+    align-items: center;  
+    justify-content: center; 
     cursor: pointer;
-    border-radius:10%;
-    background-color: var(--ligth-purple);
+    border-radius: 10%;
     margin-right: 20px;
+    background-color: var(--ligth-purple);
+    padding: 20px; /* Adicionando padding para dar espaço entre a imagem e o texto */
 
     :hover {
         background-color: var(--medium-purple);
         transition: 0.4s;
         font-size: 1.2em;
-      }
+        width: 240px; /* Aumentando a largura no hover */
+        height: 240px; /* Ajustando a altura no hover */
+    }
+
+    @media only screen and (max-width: 640px) {
+        width: 120px;
+        height: 120px;
+    }
 `;
 
 export const Img = styled.img`
-  max-width: 20%;
-  margin-bottom: 20%;
-  @media only screen and (max-width: 640px) {
-    height: 25px;
-  }
+    max-width: 40%; /* Ajustando a largura máxima da imagem */
+    margin-bottom: 20%; /* Ajustando o espaço entre a imagem e o texto */
+    @media only screen and (max-width: 640px) {
+        height: 25px;
+    }
 `;
 
 export const CardContainer = styled.div`
--- src/pages/dentista/forms/index.js
+++ src/pages/dentista/forms/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { ContentContainer } from "../../../constants/containers/index";
 import Layout from "../../../components/layout";
-import { Subtitle } from "../../../components/texts";
+import { Subtitle, Text, SubtitleSection} from "../../../components/texts";
 import { useHistory } from "react-router-dom";
-import { Card, Img, CardContainer } from "./components/index"; // Componentes atualizados
+import { Card, Img, CardContainer } from "./components/index"; 
 import CitologiaFormIcon from "../../../assets/imgs/icones/CitologiaFormIcon.png";
 import OdontoOsseoIcon from "../../../assets/imgs/icones/OdontoOsseoIcon.png";
 
@@ -24,11 +24,11 @@ const Forms = () => {
                 <CardContainer>
                     <Card onClick={() => handleFormSelection("/dentista/cistoPatologico")}>
                         <Img src={CitologiaFormIcon} />
-                        <p>Formulário CistoPatologico</p>
+                        <SubtitleSection>Formulário CistoPatologico</SubtitleSection>
                     </Card>
                     <Card onClick={() => handleFormSelection("/dentista/odontoOsseo")}>
                         <Img src={OdontoOsseoIcon} /> 
-                        <p>Formulário Ósseo Odontológico</p>
+                        <SubtitleSection>Formulário ÓsseoOdontológico</SubtitleSection>
                     </Card>
                 </CardContainer>
             </ContentContainer>
